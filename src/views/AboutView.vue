54b2ee5a95c5f113098617a82eeea9c5b648c298
Shivani Bhakta <sdpatel0612@gmail.com>
2023-09-06 14:59:47 +0200

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    {{ name }}
+  </div>
+  <div>{{ color }}</div>
+  <div>{{ type }}</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: 'N/a',
+      color: 'N/a',
+      type: 'N/a'
+    }
+  },
+  async created() {
+    console.log(this.$route)
+    const nameParam = this.$route.query.name
+    const response = await fetch('http://localhost:3000/animals/' + nameParam)
+    const result = await response.json()
+
+    this.name = result.name
+    this.color = result.color
+    this.type = result.type
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li v-for="animal in animalsList">
+      <RouterLink :to="`/about?name=${animal.name}`">{{ animal.name }}</RouterLink>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      animalsList: []
+    }
+  },
+  async created() {
+    const response = await fetch('http://localhost:3000/animals/')
+    const result = await response.json()
+    this.animalsList = result.results
+  }
+}
+</script>
